89c21aebfc61aa67654c6b29100beec146cbd86c
mozzaavanela <mozzavanella12@gmail.com>
2024-08-29 17:49:44 +0700

    membuat database

---
+++ database/migrations/2024_08_29_102841_create_wargas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wargas', function (Blueprint $table) {
+            $table->id();
+            $table->date('id_daerah');
+            $table->string('name');
+            $table->string('umur');
+            $table->string('status');
+            $table->string('pekerjaan');
+            $table->string('penghasilan_tetap');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wargas');
+    }
+};
